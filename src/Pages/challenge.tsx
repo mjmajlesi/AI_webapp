3277c66c431d5196a2198c608a865c31cbd4293f
محمد جواد مجلسی <mohammadjavadmajlesi313@gmail.com>
2025-07-02 23:31:52 +0330

    add setting and state in Challenge component for build page

--- src/Pages/challenge.tsx
+++ src/Pages/challenge.tsx
@@ -1,10 +1,25 @@
-import React from 'react'
-import Navbar from '../Components/Navbar'
+import React, { useState } from 'react'
 
 function challenge() {
+
+  const [challenge , SetChallenge] = useState(null)
+  const [loading , SetLoading] = useState<boolean>(false)
+  const [error , SetError] = useState(null)
+  const [difficulty , SetDifficulty] = useState<string>("easy")
+  const [quote , SetQuote] = useState(null)
+
+  const fetchQute = async () => {}
+  const genarateChallenge = async () => {}
+  const getNextRestTime = () => {}
+
+
   return (
+    <>
     <div>
+      <h2>Coding Challenge Generator</h2>
+
     </div>
+    </>
   )
 }
 
